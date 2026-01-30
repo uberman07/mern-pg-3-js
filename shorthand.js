a53db236adbe859cb9718d1ad986f134731616f6
uberman07 <saftyobaid7@gmail.com>
2026-01-30 22:40:16 +0600

    add shorthand.js with variable operations and console logs

---
+++ shorthand.js
@@ -0,0 +1,35 @@
+var price = 34;
+// price = price = 39;
+price += 10;
+console.log(price)
+
+var frig = 452389045;
+
+frig-= 234234;
+
+console.log(frig)
+
+var saf = 34;
+saf *= 4;
+
+console.log(saf)
+
+console.log(40 + '343')
+
+var first = 'safty';
+var second = 'obaid';
+
+var fullName = first + second
+
+console.log(fullName)
+
+console.log('40'-34)
+console.log(40-'34')
+
+let x = '10';
+let num = Number(x)
+console.log(num)
+console.log(x)
+
+const a = isNaN(9);
+console.log(a)
