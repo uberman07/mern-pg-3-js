809dfaad4d30257ea02fae4c55f845835ccc4a16
uberman07 <saftyobaid7@gmail.com>
2026-01-30 21:58:18 +0600

    learnt types of variable

---
+++ type.js
@@ -0,0 +1,20 @@
+// string type variable
+
+var name = "safty";
+var name2 = "safty obaid rahman"
+
+console.log(name2)
+
+console.log(name);
+console.log(typeof name2)
+
+
+// boolean type
+var isRich = true;
+console.log(isRich)
+
+var pass = false;
+
+console.log(pass)
+
+console.log(typeof pass)
--- variable.js
+++ variable.js
@@ -3,7 +3,7 @@ var weight = 30;
 var price = 34;
 price = 600; 
 var year = 34;
-
+year = 456;
 var age;
 
 console.log(year);
