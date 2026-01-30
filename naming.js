f4fb2cd383f360bf70e3f394ff3915e605a274a7
uberman07 <saftyobaid7@gmail.com>
2026-01-30 22:21:03 +0600

    add variable naming conventions and examples

---
+++ naming.js
@@ -0,0 +1,13 @@
+// 1. No keyword in variable name
+// no space or gap in variable name
+// no quote
+// can not start with a number , but number as other than first letter allowed
+// name is case sensitive
+
+// how to write a long variable name
+// write_in_this_way,snake case
+// writeInThisWay, camel case
+// WriteInThisWay , pascal case
+
+
+
---
+++ number.js
@@ -0,0 +1,24 @@
+var orangePrice = 20;
+var chocolate = 9.4;
+var applePrice = parseInt('34');
+console.log(typeof applePrice)
+console.log(typeof chocolate)
+console.log(typeof orangePrice)
+console.log(apple)
+console.log(orangePrice + chocolate)
+console.log(orangePrice + applePrice)
+
+var apple = parseInt('sdfl')
+console.log(typeof apple)
+
+console.log(apple+applePrice)
+
+var first = 0.1;
+var second = 0.2;
+var total = first + second;
+console.log(total)
+
+console.log(total.toFixed(3))
+
+var num = 56/0;
+console.log(num)
