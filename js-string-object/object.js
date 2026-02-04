526c74549021b343d13b1031d61f30eb7d881e01
uberman07 <saftyobaid7@gmail.com>
2026-02-04 23:51:04 +0600

    add object example demonstrating non-primitive data type

---
+++ js-string-object/object.js
@@ -0,0 +1,15 @@
+// object
+// non-primitive data type
+
+const bottle = {
+    brand: 'apple',
+    price: 34,
+    color: 'white',
+    isClean: false,
+    av: ['fe', 'sa', 'hi'],
+    size : {
+        small: '250ml',
+        medium: '500ml',
+        large: '1000ml'
+    }
+}
